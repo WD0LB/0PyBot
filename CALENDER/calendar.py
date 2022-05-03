f895a1245d91b36c69da7fcc7676ed29eaa86147
WD0LB <wd.laanibi@gmail.com>
2022-05-03 16:27:08 +0000

    Adding CALENDAR

---
+++ CALENDER/calendar.py
@@ -0,0 +1,6 @@
+#Calendar.py
+#Youness & Walid
+#Essential target: create a GUI Calendar with basic functionalities
+#Compliment target: integrate Calendar with 0PyBot voice functinality
+
+#rules bro: comments comments comments :)
