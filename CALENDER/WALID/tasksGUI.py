640f10a92dc004e8acb2cd4055e27ce313a5a979
WD0LB <wd.laanibi@gmail.com>
2022-05-14 15:22:39 +0100

    advance in tasks gui

--- CALENDER/WALID/tasksGUI.py
+++ CALENDER/WALID/tasksGUI.py
@@ -1,8 +1,11 @@
 from tkinter import *
-# from tasks import tasksList
+from tasks import tasksList
 def taskGUI(tasksList):
     # Create The Gui Object
     tk = Tk()
+
+    # Set the geometry of the GUI Interface
+    tk.geometry("500x500")
 
     # Adding the Button and Label
     def add_task():
@@ -11,24 +14,36 @@ def taskGUI(tasksList):
         # task.pack(pady = 20)
         print("value",value)
         print("entree", entree)
-        Button(tk, text = "add task",
-        command = add_task).pack(pady = 20)
+    frame = Frame(tk)
+    frame.pack(pady=20)
 
-    # Set the geometry of the GUI Interface
-    tk.geometry("500x500")
+    Button(frame, text = "add task",
+        command = add_task).pack(pady = 20, side = RIGHT)
 
-    tasksList = tasksList
-    tasksStr = '\n'.join(tasksList)
-    print(tasksStr)
-
-    tasks = Label(tk, text = tasksStr)
-
-    tasks.pack(pady = 20)
     # entrée
     value = StringVar()
     value.set("texte par défaut")
-    entree = Entry(tk, textvariable=value , width=30)
-    entree.pack(pady = 20)
+    entree = Entry(frame, textvariable=value , width=30)
+    entree.pack(pady = 20, side = LEFT)
+
+    tasks = Frame(tk).pack(pady=20)
+    # frm = Label(tasks, text="test").pack(pady = 20, side = LEFT)
+    # btn = Button(tasks, text = "remove").pack(pady=20, side= RIGHT)
+    frm = []
+    btn = []
+    taskLabel = []
+    i = 0
+    for task in tasksList:
+        taskLabel.append(Label(tasks).pack())
+        frm.append(Label(taskLabel[i], text=task).pack(side = LEFT))
+        btn.append(Button(taskLabel[i], text = "remove").pack(side= RIGHT))
+        i+=1
+        print(i)
+        print(task)
+
+
 
     # Execute Tkinter
     tk.mainloop()
+
+taskGUI(tasksList)
